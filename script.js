c6b3b95f44198d33c1b31396b17cb596bcf0a885
Ricellyo Cruz Silva <ricellyocruzsilva@gmail.com.br>
2025-10-31 21:29:58 -0300

    feature de remoção da tela inplementada

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ Lista de tarefas
 [x] Saber quando o botão foi clicado
 [x] Pegar o texto dentro do input
 [ ] colocar esse texto na tela
-[ ] Deletar a tarefa. 
+[ ] Deletar a tarefa (Quando clicar no X)
  
 */ 
 
@@ -11,9 +11,15 @@ function adicionarTarefa(){
     let input = document.querySelector("input").value;
 
     let li = document.createElement("li");
-    li.innerHTML = input  + "<span>❌</span>";
+    li.innerHTML = input  + '<span onclick="deletarTarefas(this)">❌</span>';
 
     document.querySelector("ul").appendChild(li);
 
-    input = "";
+    document.querySelector("input").value = '';
+}
+
+function deletarTarefas(li){
+    
+    li.parentElement.remove()
+
 }
